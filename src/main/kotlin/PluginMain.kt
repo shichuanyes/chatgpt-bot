bc9359c38f666dfff4f6818edac3f7dc926d093b
shichuanyes <shichuanyes@gmail.com>
2023-03-02 19:57:21 -0500

    Delete sample code

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -15,13 +15,7 @@ import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.contact.Member
 import net.mamoe.mirai.contact.User
 import net.mamoe.mirai.event.GlobalEventChannel
-import net.mamoe.mirai.event.events.BotInvitedJoinGroupRequestEvent
-import net.mamoe.mirai.event.events.FriendMessageEvent
 import net.mamoe.mirai.event.events.GroupMessageEvent
-import net.mamoe.mirai.event.events.NewFriendRequestEvent
-import net.mamoe.mirai.message.data.Image
-import net.mamoe.mirai.message.data.Image.Key.queryUrl
-import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.utils.info
 
 /**
@@ -67,33 +61,6 @@ object PluginMain : KotlinPlugin(
         //配置文件目录 "${dataFolder.absolutePath}/"
         val eventChannel = GlobalEventChannel.parentScope(this)
         eventChannel.subscribeAlways<GroupMessageEvent> {
-//            //群消息
-//            //复读示例
-//            if (message.contentToString().startsWith("复读")) {
-//                group.sendMessage(message.contentToString().replace("复读", ""))
-//            }
-//            if (message.contentToString() == "hi") {
-//                //群内发送
-//                group.sendMessage("hi")
-//                //向发送者私聊发送消息
-//                sender.sendMessage("hi")
-//                //不继续处理
-//                return@subscribeAlways
-//            }
-//            //分类示例
-//            message.forEach {
-//                //循环每个元素在消息里
-//                if (it is Image) {
-//                    //如果消息这一部分是图片
-//                    val url = it.queryUrl()
-//                    group.sendMessage("图片，下载地址$url")
-//                }
-//                if (it is PlainText) {
-//                    //如果消息这一部分是纯文本
-//                    group.sendMessage("纯文本，内容:${it.content}")
-//                }
-//            }
-
             if (message.contentToString().startsWith(("!ask"))) {
                 val body = RequestJson(messages = arrayListOf<Message>(Message(role = "user", content = message.contentToString().replace("!ask", ""))))
                 val (_, response, result) = Fuel.post("https://api.openai.com/v1/chat/completions")
@@ -105,18 +72,6 @@ object PluginMain : KotlinPlugin(
                 val json = gson.fromJson(response.data.decodeToString(), ResponseJson::class.java)
                 group.sendMessage(json.choices.first().message.content)
             }
-        }
-        eventChannel.subscribeAlways<FriendMessageEvent> {
-            //好友信息
-            sender.sendMessage("hi")
-        }
-        eventChannel.subscribeAlways<NewFriendRequestEvent> {
-            //自动同意好友申请
-            accept()
-        }
-        eventChannel.subscribeAlways<BotInvitedJoinGroupRequestEvent> {
-            //自动同意加群申请
-            accept()
         }
 
         modPermission // 注册权限
